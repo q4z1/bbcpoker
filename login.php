95483e54c41e035b4f58503b55d56b0eb9654a37
supinf0 <f0x01@web.de>
2017-04-14 04:37:37 +0200

    i had typos in my last commit

--- login.php
+++ login.php
@@ -104,9 +104,9 @@ if($goodlogin==1)
 <li><a href="exp5/awards/awinput1.php">Add Awards </a></li>
 <li><a href="exp4/admin2.php">Manage BBC Accounts</a></li>
 <li><a href="exp3/bbcbotmanual.php">View bbcbot commands</a></li>
-<li><a href="exp6/dates1.php">Create dates<a/></li>
-<li><a href="exp6/dates3.php?d=0">View dates<a/></li>
-<li><a href="exp6/dates3.php?d=1">Delete dates<a/></li>
+<li><a href="exp6/dates1.php">Create dates</a></li>
+<li><a href="exp6/dates3.php?d=0">View dates</a></li>
+<li><a href="exp6/dates3.php?d=1">Delete dates</a></li>
 <li><a href="exp4/shoutbox3.php">View deleted Messages</a></li>
 E;
   if($upc==2)
@@ -114,9 +114,9 @@ E;
 <li><a href="exp6/upload1.php">Upload and Input a BBC Game</a></li>
 <li><a href="exp2/test1.php">Input without logfile</a></li>
 <li><a href="exp4/shoutbox1.php?admin=1">Talk to people</a></li>
-<li><a href="exp6/dates1.php">Create dates<a/></li>
-<li><a href="exp6/dates3.php?d=0">View dates<a/></li>
-<li><a href="exp6/dates3.php?d=1">Delete dates<a/></li>
+<li><a href="exp6/dates1.php">Create dates</a></li>
+<li><a href="exp6/dates3.php?d=0">View dates</a></li>
+<li><a href="exp6/dates3.php?d=1">Delete dates</a></li>
 <li><a href="exp3/bbcbotmanual.php">View bbcbot commands</a></li>
 <li><a href="exp4/shoutbox3.php">View deleted Messages</a></li>
 E;
@@ -145,7 +145,7 @@ E;
   {
     print "<li>".$row->name."</li>\n";
   }
-  print "</ul></p>";
+  print "</ul>";
   
 }
 
